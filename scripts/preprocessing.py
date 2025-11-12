a4da69a2417d3e6edf18f8a902c26af1cd2be860
rhy321 <rhyahm25@gmail.com>
2025-11-12 17:41:40 +0530

    phase 2: EDA added

--- scripts/preprocessing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-flights = pd.read_csv('data/Flights.csv')
-airlines = pd.read_csv('data/Airlines.csv')
-airports = pd.read_csv('data/Airports.csv')
-
-flights.info()
-flights.head()
-flights.isnull().sum()
